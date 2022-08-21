199c2a4c4d92462eef91d77afcf9e479e5df5851
Muhammad Usman <usmanmanzoor@troontechnologies.com>
2022-08-21 16:50:38 +0500

    Test Code

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //input = { "john": 23, "james": 24, "vincent": 34, "louis": 29}
+            //Sort by keys, and return array of values only.
+
+            IDictionary<int, string> numberNames = new Dictionary<int, string>() { {  23, "john" } ,{ 24 , "james" },{  34, "vincent" }, {  29, "louis" } };
+
+            var list = new List<ArraySort>();
+            foreach (var item in numberNames)
+            {
+               var obj = new ArraySort{
+                    Key = Convert.ToInt32(item.Key),
+                    value = item.Value.ToString()
+                };
+
+
+                list.Add(obj);
+
+            }
+            var res = list.OrderBy(c => c.Key).ToList();
+            //Console.WriteLine(numberNames.Keys)
+
+            foreach (var item in res)
+            {
+                Console.WriteLine(item.value);
+            }
+       
+        }
+
+    }
+    public class ArraySort {
+        public int Key { get; set; }
+        public string value { get; set; }
+    }
+}
